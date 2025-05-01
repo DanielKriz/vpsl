0d6a0b91fb76094729a78f5b8b4131b565f94f01
Daniel Kříž <daniel.kriz@protonmail.com>
2025-05-01 11:32:39 +0200

    feat: add an empty prototype for texture class

---
+++ include/vp/builder_graph/objects/texture_object.hpp
@@ -0,0 +1,13 @@
+#ifndef VP_BUILDER_GRAPH_TEXTURE_OBJECT_HPP
+#define VP_BUILDER_GRAPH_TEXTURE_OBJECT_HPP
+
+namespace vp {
+
+class TextureObject {
+public:
+private:
+};
+
+} // namespace vp
+
+#endif // VP_BUILDER_GRAPH_TEXTURE_OBJECT_HPP
