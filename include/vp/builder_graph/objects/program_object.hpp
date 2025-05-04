5683d3c1a5b9459312ea9d9da6254e4e727b84a1
Daniel Kříž <daniel.kriz@protonmail.com>
2025-05-05 01:11:39 +0200

    feat(ProgramObject): init

--- include/vp/builder_graph/objects/program_object.hpp
+++ include/vp/builder_graph/objects/program_object.hpp
@@ -2,10 +2,23 @@
 #define VP_BUILDER_GRAPH_PROGRAM_OBJECT_HPP
 
 #include <vp/builder_graph/objects/builder_object_interface.hpp>
+#include <vp/types.hpp>
 
 namespace vp {
 
-class ProgramObject : public IBuilderGraphObject {};
+class ProgramObject : public IBuilderGraphObject {
+public:
+    explicit ProgramObject(std::string name) : m_id(++m_programCounter), m_name(std::move(name)) { };
+    ProgramObject() : m_id(++m_programCounter) { m_name = std::to_string(m_id); }
+
+    [[nodiscard]] const std::string &getName() const noexcept {
+        return m_name;
+    }
+private:
+    inline static u64 m_programCounter { 0 };
+    u64 m_id;
+    std::string m_name;
+};
 
 } // namespace vp
 
