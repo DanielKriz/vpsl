a4823a79dcd475e0e243941d7a2fcd268462eec0
Daniel Kříž <daniel.kriz@protonmail.com>
2025-05-05 01:09:02 +0200

    feat(Parser): add method for construction of directives

--- include/vp/interpreter/parser.hpp
+++ include/vp/interpreter/parser.hpp
@@ -2,10 +2,13 @@
 #define VP_INTERPRETER_PARSER_HPP
 
 #include <stack>
+#include <memory>
 
 #include <vp/builder_graph/graph.hpp>
 #include <vp/dependency_graph.hpp>
 #include <vp/interpreter/parser_stages.hpp>
+#include <vp/interpreter/directive.hpp>
+#include <vp/interpreter/token.hpp>
 
 namespace vp {
 
@@ -30,6 +33,26 @@ public:
     Parser &operator=(const Parser &other) = delete;
     Parser(Parser &&other) = delete;
     Parser &operator=(Parser &&other) = delete;
+
+    // TODO: make this optional
+    static std::unique_ptr<IDirective> constructDirectiveFromToken(const Token &token) {
+        TokenKind kind = token.getTokenKind();
+        switch (kind) {
+        case TokenKind::ShaderDirective:
+            return std::make_unique<ShaderDirective>();
+        case TokenKind::ProgramDirective:
+            return std::make_unique<ProgramDirective>();
+        case TokenKind::LoadDirective:
+            return std::make_unique<LoadDirective>();
+        case TokenKind::BeginDirective:
+            return std::make_unique<BeginDirective>();
+        case TokenKind::EndDirective:
+            return std::make_unique<EndDirective>();
+        case TokenKind::TextureDirective:
+        default:
+            return nullptr;
+        }
+    }
 
 private:
     vp::DependencyGraph m_shaderDependencyGraph;
