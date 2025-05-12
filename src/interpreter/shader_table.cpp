87ec64a642396ccf7635090b2e16fc5ace1e18f2
Daniel Kříž <daniel.kriz@protonmail.com>
2025-05-12 16:10:08 +0200

    chore: remove unused classes and dead code

--- src/interpreter/shader_table.cpp
+++
@@ -1,9 +0,0 @@
-#include <vp/interpreter/shader_table.hpp>
-
-namespace vp {
-
-void ShaderTable::add(const std::string &name, ShaderBuilder &builder) {
-    ShaderTable::getInstance().m_map.emplace(name, builder);
-}
-
-} // namespace vp
