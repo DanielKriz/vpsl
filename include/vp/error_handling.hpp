9d01cf2c2becc0d3dbff2b3f10e12c8155bf2c72
Daniel Kříž <daniel.kriz@protonmail.com>
2025-05-13 21:15:19 +0200

    fix: include order

--- include/vp/error_handling.hpp
+++ include/vp/error_handling.hpp
@@ -1,7 +1,7 @@
 #ifndef VP_ERROR_HANDLING_HPP
 #define VP_ERROR_HANDLING_HPP
 
-#include <vp/error_handling/error_handler.hpp>
 #include <vp/error_handling/default_error_reporter.hpp>
+#include <vp/error_handling/error_handler.hpp>
 
 #endif // VP_ERROR_HANDLING
