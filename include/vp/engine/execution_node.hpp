d2b0a8258cab54b3ee0b3287c6c33a8b5b6109a6
Daniel Kříž <daniel.kriz@protonmail.com>
2025-05-14 17:26:08 +0200

    feat(ExecutionNode): add support for options

--- include/vp/engine/execution_node.hpp
+++ include/vp/engine/execution_node.hpp
@@ -2,6 +2,8 @@
 #define VP_ENGINE_EXECUTION_NODE_HPP
 
 #include <memory>
+
+#include <vp/description/options.hpp>
 #include <vp/graphics/opengl/program.hpp>
 
 namespace vp {
@@ -10,8 +12,11 @@ class ExecutionNode {
 public:
     ExecutionNode();
     [[nodiscard]] gl::opengl::Program &getProgram() noexcept { return *m_pProgram; }
+    void applyContext() const noexcept;
+    void setOptions(const Options &opts);
 private:
     std::shared_ptr<gl::opengl::Program> m_pProgram;
+    std::unique_ptr<Options> m_pOptions;
 };
 
 } // namespace vp
--- src/engine/execution_node.cpp
+++ src/engine/execution_node.cpp
@@ -1,8 +1,39 @@
 #include <vp/engine/execution_node.hpp>
+#include <vp/graphics/context_cache.hpp>
 
 namespace vp {
 
-ExecutionNode::ExecutionNode() : m_pProgram(std::make_shared<gl::opengl::Program>()) {
+ExecutionNode::ExecutionNode()
+    : m_pProgram(std::make_shared<gl::opengl::Program>())
+    , m_pOptions(std::make_unique<Options>()) {
+}
+
+void ExecutionNode::setOptions(const Options &opts) {
+    *m_pOptions = opts;
+}
+
+void ExecutionNode::applyContext() const noexcept {
+    using namespace gl;
+    auto &cache = ContextCache::getInstance();
+
+    auto &opts = *m_pOptions;
+
+    cache.enableDepthTest(opts.isDepthTestEnabled);
+    cache.enableBlending(opts.isDepthTestEnabled);
+    cache.enableFaceCulling(opts.isDepthTestEnabled);
+
+    if (opts.frontFacePolygonMode == opts.backFacePolygonMode) {
+        cache.setPolygonMode(opts.frontFacePolygonMode);
+    } else {
+        cache.setPolygonMode(Face::Front, opts.frontFacePolygonMode);
+        cache.setPolygonMode(Face::Back, opts.backFacePolygonMode);
+    }
+
+    cache.setCullingMode(opts.cullingMode);
+    cache.setFrontFaceMode(opts.frontFaceMode);
+    cache.setDepthFunction(opts.depthFunction);
+    cache.setSrcBlendFactor(opts.srcBlendFactor);
+    cache.setDstBlendFactor(opts.dstBlendFactor);
 }
 
 } // namespace vp
